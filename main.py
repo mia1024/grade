64c916aeb9adc76e0557683bde000991dd7a9240
Mia Celeste <mia@miaceleste.dev>
2021-02-19 00:32:43 -0800

    Fixed a issue with clock

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ for key in sorted(d.keys()):
     print('-'*78)
     print(f'{"Assignments due on "+key.strftime("%A, %b %-d"): ^78}')
     for a in d[key]:
-        print(f'{a.name} ({a.course.name}): {a.deadline.strftime("%-H:%M %p")}')
+        print(f'{a.name} ({a.course.name}): {a.deadline.strftime("%-I:%M %p")}')
